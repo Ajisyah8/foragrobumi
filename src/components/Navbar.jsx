edfe5b5d1a05bab05bb49c9db2e1ca0294ab1a62
Ajisyah8 <ajeajeaje08@gmail.com>
2024-11-10 22:03:38 +0700

    foragrobumi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LoginIcon } from '@heroicons/react/solid';  // Import ikon pintu masuk dari Heroicons
 
 function Navbar() {
     return (
@@ -15,16 +16,18 @@ function Navbar() {
                 </div>
             </div>
 
-           
             <div className="flex flex-grow justify-center space-x-11 text-sm text-blue-600">
                 <a href="#" className="hover:text-green-600">Beranda</a>
-                <a href="#" className="hover:text-green-600">Events</a>
-                <a href="#" className="hover:text-green-600">Booking</a>
+                <a href="#" className="hover:text-green-600">Destinations</a>
+                <a href="#" className="hover:text-green-600">About us</a>
             </div>
 
-       
-            <div className="text-sm text-blue-600">
-                <a href="#" className="hover:text-green-600">About Us</a>
+            {/* Bagian Login sebagai tombol */}
+            <div className="text-sm">
+                <a href="#" className="flex items-center space-x-2 px-4 py-2 bg-green-600 text-white rounded-full hover:bg-green-700 transition-all">
+                    <LoginIcon className="h-5 w-5" />
+                    <span>Login</span>
+                </a>
             </div>
         </div>
     );
